a47e21a76cf6d844d6d1421a28cb483c27298b9b
river040118 <river040118@gmail.com>
2025-08-21 17:44:07 +0900

    [Section 3] 변수 값 변경

---
+++ src/variable/Var3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        int a;
+        a = 10; // 변수 초기화
+        System.out.println(a);
+        a = 50; // 변수 변경
+        System.out.println(a);
+    }
+}
