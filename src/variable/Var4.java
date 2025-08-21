127b677c22e8118e115985e7d710e1bd39c715d9
river040118 <river040118@gmail.com>
2025-08-21 17:54:51 +0900

    [Section 3] 변수 선언과 초기화

---
+++ src/variable/Var4.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        int a;
+        int b; // 변수는 하니씩 선언 할 수 도 있고
+
+        int c, d; // 한번에 두개를 선언할 수도 있다
+    }
+}
---
+++ src/variable/Var5.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class Var5 {
+    public static void main(String[] args) {
+        int a;
+        a = 1; // 변수 선언과 초기화를 분리
+        System.out.println(a);
+
+        int b = 2; // 변수 선언과 동시에 초기화
+        System.out.println(b);
+
+        int c = 3, d = 4; // 한번에 여러 변수를 선언하고 초기화
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
