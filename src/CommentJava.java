249e7d17b67d51b32ef5b55e4bb1b27b9d089cd6
river040118 <river040118@gmail.com>
2025-08-21 17:04:27 +0900

    [Section 2] Comment Java

---
+++ src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+    public static void main(String[] args){
+        System.out.println("Hello Java");
+        // System.out.println("Hello Java2"); // 한 줄 주석
+        /* 여러 줄 주석
+        System.out.println("Hello Java3");
+        System.out.println("Hello Java3");
+         */
+        System.out.println("Hello Java4");
+    }
+}
+
