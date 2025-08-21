bad4d4aca91cab2da52d042961c61ffb9081fc51
river040118 <river040118@gmail.com>
2025-08-21 17:41:10 +0900

    [Section 3] 변수 시작

---
+++ src/variable/Var1.java
@@ -0,0 +1,10 @@
+package variable; // 패키지 내에 자바 파일을 만들었다면 첫 줄에는 꼭 패키지를 선언해주어야 함!
+                  // 자바 파일 위치와 패키지 선언의 위치가 같아야 한다.
+
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println("10");
+        System.out.println("10");
+        System.out.println("10");
+    }
+}
